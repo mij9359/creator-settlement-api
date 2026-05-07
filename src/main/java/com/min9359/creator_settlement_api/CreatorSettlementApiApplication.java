0c7514afbcf862ad6c56c897663f55d5aa9f61a5
mij9359 <mij9359@gmail.com>
2026-05-07 20:39:04 +0900

    chore: inital Spring Boot project setup with MyBatis

---
+++ src/main/java/com/min9359/creator_settlement_api/CreatorSettlementApiApplication.java
@@ -0,0 +1,13 @@
+package com.min9359.creator_settlement_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatorSettlementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatorSettlementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/min9359/creator_settlement_api/CreatorSettlementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.min9359.creator_settlement_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatorSettlementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
